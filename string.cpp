43f79453566768161440e269aaddc8c51c94b674
dips6202 <deepikakumari6202@gmail.com>
2022-03-02 22:30:39 +0530

    initial commit

---
+++ string.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string a,b;
+    char c1,c2;
+    
+    cin>>a>>b;
+    
+    int l1=a.size();
+    int l2=b.size();
+    cout<<l1<<" "<<l2<<endl;
+    
+    string c=a+b;
+    cout<<c<<endl;
+    
+     c1=b[0];
+     c2=a[0];
+     a[0]=c1;
+     b[0]=c2;
+     cout<<a<<" "<<b;
+    
+  
+    return 0;
+}
