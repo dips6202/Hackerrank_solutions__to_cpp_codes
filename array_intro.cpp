c03c684d18bb833594d76c1efa8d63dc55aed3d3
dips6202 <deepikakumari6202@gmail.com>
2022-03-22 22:59:16 +0530

    initial commi

--- array_intro.cpp
+++ array_intro.cpp
@@ -8,15 +8,12 @@ using namespace std;
 
 int main() {
     int n,i;
-    int arr[1000];
-    
+    int arr[1000];    
     cin>>n;
     for(i=0;i<n;i++)
-    cin>>arr[i];
-    
+    cin>>arr[i];    
     for(i=n-1;i>=0;i--)
-    cout<<arr[i]<<" ";
-       
+    cout<<arr[i]<<" ";       
     return 0;
 }
 
